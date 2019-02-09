a4b4305f7b4a288808ba25d5aa44a0251f48f1f3
Mobmauri33 <mau_33_@hotmail.com>
2019-02-08 21:28:48 -0300

    ejerciciosTerminados

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,11 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var numero1;
+    var numero2;
+    var rEsultado;
+    numero1 = document.getElementById("importe").value;
+    numero2 = document.getElementById("resultado").value;
+    rEsultado = parseInt(numero1)-parseInt(numero1)*0.25;
+    alert (rEsultado);
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,12 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
+    var numero1;
+    var numero2;
+    var rEsultado;
+    numero1 = document.getElementById("sueldo").value;
+    numero2 = document.getElementById("resultado").value;
+    rEsultado = parseInt(numero1)+parseInt(numero1)*0.10;
+    alert (rEsultado);
 	
 }
