cee783ce4173b0a17d27d99b75d3b44b1e66fc61
Mobmauri33 <mau_33_@hotmail.com>
2019-02-10 22:53:14 -0300

    intento de parcial1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,8 +1,11 @@
 
 function mostrar()
 {
-var medicion;
-medicion = ("5mts x 10mts");
-prompt ("ancho y el largo de un rectángulo");
-alert (medicion);
+var medicion1;
+var medicion2;
+var rEsultado;
+medicion1 = prompt ("ancho del rectangulo");
+medicion2 = prompt ("largo del rectangulo");
+rEsultado = parseInt(medicion1)+parseInt(medicion2);
+alert (rEsultado*2);
 }
