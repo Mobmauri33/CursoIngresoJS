743919992270d5d05b948294bdf1915c8f90bbcc
Mobmauri33 <mau_33_@hotmail.com>
2019-02-13 22:16:32 -0300

    if hasta el 6

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,10 @@
 function mostrar()
 {
 //tomo la edad  
-
+var edad;
+edad = document.getElementById("edad").value;
+if (edad>=18) {alert ("sos mayor de edad");
+ }
+  else {alert ("no sos mayor de edad")};
 
 }//FIN DE LA FUNCIÓN
