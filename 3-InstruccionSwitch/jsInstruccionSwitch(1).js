4e78a49fc7c060006c6ede2c322ea2854d3c483c
alumno <alumno>
2019-02-20 19:53:45 -0300

    instruccionswitch
    Signed-off-by: alumno <alumno>

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,8 +2,23 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño) {
+    case "Enero": 
+    alert("que comiences bien el año!!!");
+    break;
+    case "Marzo": 
+    alert("a clases!!!");
+    break;
+    case "Julio": 
+    alert("se vienen las vacaciones!!!");
+    break;
+    case "Diciembre": 
+    alert("Felices fiesta!!!");
+    default: 
+    alert();
+}
+}
 
 
 
-
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,8 +2,22 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
-alert (mesDelAño);
+switch(mesDelAño) {
+    case "Agosto":
+    case "Julio":
+    alert("Abrigate que hace frio");
+    break;
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+    alert("Falta para el invierno");
+    break;
+    default:
+    alert("Ya pasamos el frio, ahora calor!!!");
+}
 
 
 
