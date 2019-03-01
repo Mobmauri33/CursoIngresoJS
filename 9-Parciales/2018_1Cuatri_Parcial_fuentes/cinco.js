a5ed8774cb0ee2c0a2f807c7f2720d56e4c40ec9
Mobmauri33 <mau_33_@hotmail.com>
2019-03-01 16:38:43 -0300

    parcial6

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
-
+var planeta = prompt("Ingrese el Nombre del planeta");
+switch(planeta) {
+    case "tierra":
+    alert("acá vivimos");
+    break;
+    case "mercurio":
+    case "venus":
+    alert("acá hace más calor");
+    break;
+    case "marte":
+    case "neptuno":
+    case "jupiter":
+    case "urano":
+    case "pluton":
+    alert("acá hace más frio");
+    break;
+    default:
+    alert("ese no es un planeta");
+}
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
-
+var numero1 = prompt("Ingrese el primer número");
+var numero2 = prompt("Ingrese el segundo número");
+numero1 = parseInt(numero1);
+numero2 = parseInt(numero2);
+var resultado1;
+var resultado2;
+var resultado3;
+resultado1 = (numero1 - numero2);
+resultado2 = (numero1 + numero2);
+resultado3 = (numero1 + numero2>10);
+if (numero1==numero2) {alert("El resultado es " + numero1 + " y " + numero2 + " son iguales ")}
+if (numero1>numero2) {alert("El resultado es " + resultado1)}
+if (numero1<numero2) {alert("El resultado es " + resultado2)}
 }
